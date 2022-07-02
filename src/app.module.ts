121fd36d4d686e8c1eda18d42a69347b7e9c222c
Nikita <zharinov.nicita@gmail.com>
2022-07-02 09:36:15 +0700

    [feature/mongo] #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/valhalla-training-api'),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start() {
+  try {
+    const app = await NestFactory.create(AppModule)
+    await app.listen(3000)
+  } catch (e) {
+    console.log(e)
+  }
 }
-bootstrap();
+start()
