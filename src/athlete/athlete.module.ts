252805914ed4accbd3a704707f7753176c05fcc8
Nikita <zharinov.nicita@gmail.com>
2022-07-02 10:45:17 +0700

    [feature/athlete-module] #19

---
+++ src/athlete/athlete.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { AthleteController } from './athlete.controller'
+import { Athlete, AthleteSchema } from './athlete.schema'
+import { AthleteService } from './athlete.service'
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Athlete.name, schema: AthleteSchema }]),
+  ],
+})
+export class AthleteModule {}
