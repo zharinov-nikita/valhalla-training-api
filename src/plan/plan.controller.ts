b20e3dea64e0a0386dcf6f0361a50ed86f375a73
Nikita <zharinov.nicita@gmail.com>
2022-07-04 09:16:18 +0700

    [feature/plan] PlanControler create - метод для обработки запроси и передачи данных в сервис для создания плана

--- src/plan/plan.controller.ts
+++ src/plan/plan.controller.ts
@@ -8,40 +8,17 @@ import {
   Post,
 } from '@nestjs/common'
 import { ObjectId } from 'mongoose'
-import { CreateAthleteDto } from './dto/create-plan.dto'
-import { UpdateAthleteDto } from './dto/update-plan.dto'
-import { Athlete } from './plan.schema'
-import { AthleteService } from './plan.service'
+import { CreatePlanDto } from './dto/create-plan.dto'
+import { UpdatePlanDto } from './dto/update-plan.dto'
+import { Plan } from './plan.schema'
+import { PlanService } from './plan.service'
 
-@Controller('api/athlete')
-export class AthleteController {
-  constructor(private athleteService: AthleteService) {}
+@Controller('api/plan')
+export class PlanController {
+  constructor(private planService: PlanService) {}
 
   @Post()
-  async create(@Body() dto: CreateAthleteDto): Promise<Athlete> {
-    return await this.athleteService.create(dto)
-  }
-
-  @Get()
-  async find(): Promise<Athlete[]> {
-    return await this.athleteService.find()
-  }
-
-  @Get(':_id')
-  async findById(@Param('_id') _id: ObjectId): Promise<Athlete> {
-    return await this.athleteService.findById(_id)
-  }
-
-  @Patch(':_id')
-  async findByIdAndUpdate(
-    @Param('_id') _id: ObjectId,
-    @Body() dto: UpdateAthleteDto
-  ): Promise<Athlete> {
-    return await this.athleteService.findByIdAndUpdate(_id, dto)
-  }
-
-  @Delete(':_id')
-  async findByIdAndDelete(@Param('_id') _id: ObjectId) {
-    return await this.athleteService.findByIdAndDelete(_id)
+  async create(@Body() dto: CreatePlanDto): Promise<Plan> {
+    return await this.planService.create(dto)
   }
 }
