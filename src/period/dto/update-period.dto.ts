8bcfda6e93a8b48ecada04765408930e8ef98c3b
Nikita <zharinov.nicita@gmail.com>
2022-07-05 09:55:19 +0700

    [bugfix/schema] period

--- src/period/dto/update-period.dto.ts
+++ src/period/dto/update-period.dto.ts
@@ -1,8 +1,10 @@
 import { ObjectId } from 'mongoose'
 
 export class UpdatePeriodDto {
-  readonly name: string
+  readonly title: string
+  readonly description: string
   readonly start: string
   readonly finish: string
+  readonly status: string
   readonly planId: ObjectId
 }
