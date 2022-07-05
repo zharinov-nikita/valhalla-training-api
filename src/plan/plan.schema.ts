2f3744d57a44d9c1f5f2d0ea23e3e6cf6a6eea56
Nikita <zharinov.nicita@gmail.com>
2022-07-05 09:51:36 +0700

    [bugfix/schema] plan

--- src/plan/plan.schema.ts
+++ src/plan/plan.schema.ts
@@ -6,13 +6,19 @@ export type PlanDocument = Plan & Document
 @Schema()
 export class Plan {
   @Prop({ type: String, required: true, unique: false })
-  name: string
+  title: string
+
+  @Prop({ type: String, required: true, unique: false })
+  description: string
 
   @Prop({ type: String, required: false, unique: false })
   start: string
 
   @Prop({ type: String, required: false, unique: false })
   finish: string
+
+  @Prop({ type: String, required: true, unique: false })
+  status: string
 }
 
 export const PlanSchema = SchemaFactory.createForClass(Plan)
