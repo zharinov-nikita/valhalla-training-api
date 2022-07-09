39b16a95a9481ebbcb6040be58a2199d42add7f5
Nikita <zharinov.nicita@gmail.com>
2022-07-09 10:20:11 +0700

    [hotfix/1.4.2] AppMiddlaware chcke API_KEY

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,11 @@
-import { Module } from '@nestjs/common'
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
+import { AppMiddleware } from './app.middleware'
 import { CycleModule } from './cycle/cycle.module'
 import { PeriodModule } from './period/period.module'
 import { PlanModule } from './plan/plan.module'
@@ -22,4 +28,10 @@ import { ConfigModule } from '@nestjs/config'
     PropertyModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AppMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
