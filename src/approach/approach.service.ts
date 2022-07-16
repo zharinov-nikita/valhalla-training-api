356c3b75a449221bc09667ec44fe9f6cd4a36354
Nikita <zharinov.nicita@gmail.com>
2022-07-16 11:03:24 +0700

    feature/approach service

--- src/approach/approach.service.ts
+++ src/approach/approach.service.ts
@@ -1,40 +1,40 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model, ObjectId } from 'mongoose'
-import { Property, PropertyDocument } from './property.schema'
-import { CreatePropertyDto } from './dto/create-approach.dto'
-import { UpdatePropertyDto } from './dto/update-approach.dto'
+import { Approach, ApproachDocument } from './approach.schema'
+import { CreateApproachDto } from './dto/create-approach.dto'
+import { UpdateApproachDto } from './dto/update-approach.dto'
 
 @Injectable()
-export class PropertyService {
+export class ApproachService {
   constructor(
-    @InjectModel(Property.name) private propertyModel: Model<PropertyDocument>
+    @InjectModel(Approach.name) private approachModel: Model<ApproachDocument>
   ) {}
 
-  async create(dto: CreatePropertyDto): Promise<Property> {
-    return await this.propertyModel.create(dto)
+  async create(dto: CreateApproachDto): Promise<Approach> {
+    return await this.approachModel.create(dto)
   }
 
-  async find(): Promise<Property[]> {
-    return await this.propertyModel.find()
+  async find(): Promise<Approach[]> {
+    return await this.approachModel.find()
   }
 
-  async findByField(option = {}): Promise<Property[]> {
-    return await this.propertyModel.find({ ...option })
+  async findByField(option = {}): Promise<Approach[]> {
+    return await this.approachModel.find({ ...option })
   }
 
-  async findById(_id: ObjectId): Promise<Property> {
-    return await this.propertyModel.findById({ _id })
+  async findById(_id: ObjectId): Promise<Approach> {
+    return await this.approachModel.findById({ _id })
   }
 
   async findByIdAndUpdate(
     _id: ObjectId,
-    dto: UpdatePropertyDto
-  ): Promise<Property> {
-    return await this.propertyModel.findByIdAndUpdate(_id, dto, { new: true })
+    dto: UpdateApproachDto
+  ): Promise<Approach> {
+    return await this.approachModel.findByIdAndUpdate(_id, dto, { new: true })
   }
 
-  async findByIdAndDelete(_id: ObjectId): Promise<Property> {
-    return await this.propertyModel.findByIdAndDelete({ _id })
+  async findByIdAndDelete(_id: ObjectId): Promise<Approach> {
+    return await this.approachModel.findByIdAndDelete({ _id })
   }
 }
